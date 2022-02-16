44c14ba25f7047b69a5fd66ed0f3c843b4609909
MysikM <46257801+MysikM@users.noreply.github.com>
2022-02-16 18:14:17 +0200

    Add files via upload

---
+++ precache-manifest.1bb38ceeb0e68811b78a055d6e5a378e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad24b89d42ac6dc722ac23ab104bea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3cae47d59253962e3ab",
+    "url": "/static/css/main.1020b400.chunk.css"
+  },
+  {
+    "revision": "f58c7e605fe50fc18cb2",
+    "url": "/static/js/2.fce3c684.chunk.js"
+  },
+  {
+    "revision": "f3cae47d59253962e3ab",
+    "url": "/static/js/main.ce59065d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "56c4dcf7719731742dfa1db45a776faa",
+    "url": "/static/media/logo.56c4dcf7.svg"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/static/media/possibility.3c84ae26.png"
+  }
+]);
